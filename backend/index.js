637999fb044e533ff9c2023ea4e9c886972f4d00
JosuerBague <josuer.online.work@gmail.com>
2022-09-08 21:03:45 +0800

    Add changevalue, hanldeZero, and handleChange functions to numerical buttons.
    The following functions were created to help handle input:
    changeValue, handleZero, handleChange.
    
    changeValue - An encompassing function that calls hanzleZero or
                  handleChange based on the value of the numerical button
                  presed.
    
    handleZero - Handles the change if the button pressed is a zero.
    
    handleChange - Handles the change when the button pressed is not zero.
    
    Created a global state object with the following properties:
    total, memoryTotal, valueOne, valueTwo, operation, lastkeyPressed.

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
+// Create global state object:
+const state = {
+  total: 0,
+  memoryTotal: 0,
+  valueOne: 0,
+  valueTwo: 0,
+  operation: null,
+  lastKeyPressed: null
+}
+
 // Power button
 let isPowered = false;
 const powerButton = document.querySelector(".fn-power");
@@ -16,4 +26,41 @@ function togglePower() {
   }
 
   isPowered = !isPowered;
+}
+
+// Numerical buttons
+const numericalButtons = document.querySelectorAll("[class*=num]");
+numericalButtons.forEach(button => {
+  button.addEventListener('click', changeValue)
+})
+
+function changeValue(e) {
+  const value = e.target.textContent;
+
+  // 1) First, check if the number clicked is zero.
+  if (+value === 0) {
+    // 2) If the number clicked is zero, call handleZero.
+    handleZero()
+  } else {
+    // 3) If the number clicked is not zero, call handleChange.
+    handleChange(value)
+  }
+}
+
+function handleChange(val) {
+  const operators = ["op-plus", "op-minus", "op-multiply", "op-divide"]
+  // 1) Check if the last key pressed was an operator.
+  if (operators.includes(state.lastKeyPressed)) {
+    // 2) If the last key pressed was an operator, then change valueTwo.
+    state.valueTwo = state.valueTwo ? state.valueTwo + val : val;
+  } else {
+    // 3) If the last key pressed was not an operator, change valueOne.
+    state.valueOne = state.valueOne ? state.valueOne + val : val;
+  }
+
+  console.log("val1: ", state.valueOne, "val2: ", state.valueTwo);
+}
+
+function handleZero() {
+  console.log("zeroed")
 }
