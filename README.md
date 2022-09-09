ecf23822016b38071522318211318e2152a59689
JosuerBague <josuer.online.work@gmail.com>
2022-09-09 17:15:05 +0800

    Refactor functions for numerical buttons, decimal buttons and power button

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # Here are some use cases (abilities your project needs to have):
 
-- [] Your calculator is going to contain functions for all of the basic math 
+- [x] Your calculator is going to contain functions for all of the basic math 
   operators you typically find on simple calculators, so start by creating 
   functions for the following items and testing them in your browser’s console.
       
-      - [] add
-      - [] subtract
-      - [] multiply
-      - [] divide
+      - [x] add
+      - [x] subtract
+      - [x] multiply
+      - [x] divide
   
-- [] Create a new function operate that takes an operator and 2 numbers and then calls 
-     one of the above functions on the numbers.
+- [] Create a new function 'operate' that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
 - [x] Create a basic HTML calculator with buttons for each digit, each of the above
       functions and an “Equals” key. Do not worry about wiring up the JS just yet.
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
-// Create global state object:
-const state = {
+// Create global STATE object:
+const STATE = {
   total: 0,
   memoryTotal: 0,
   valueOne: 0,
   valueTwo: 0,
   operation: null,
-  lastKeyAnOperator: false // Tracks if the last key pressed was an operator
+  lastKeyAnOperator: false, // Tracks if the last key pressed was an operator
+  isPowered: false,
 }
 
 // Power button
-let isPowered = false;
 const powerButton = document.querySelector(".fn-power");
 powerButton.addEventListener("click", togglePower);
 
@@ -31,51 +31,35 @@ function togglePower() {
 // Numerical buttons
 const numericalButtons = document.querySelectorAll("[class*=num]");
 numericalButtons.forEach(button => {
-  button.addEventListener('click', changeValue)
+  button.addEventListener('click', handleChangeValue)
 })
 
-function changeValue(e) {
-  const value = e.target.textContent;
-
-  // 1) First, check if the number clicked is zero.
-  if (+value === 0) {
-    // 2) If the number clicked is zero, call handleZero.
-    handleZero()
-  } else {
-    // 3) If the number clicked is not zero, call handleChange.
-    handleChange(value)
-  }
-
+function handleChangeValue(e) {
+  // Check if the number clicked is zero.
+  +value === 0 ? handleZero() : handleNonZero(e.target.textContent);
   handleDisplay();
-  state.lastKeyAnOperator = false;
+  STATE.lastKeyAnOperator = false;
 }
 
-function handleChange(val) {
-  // 1) Check if state.operator is not null.
-  if (state.operation) {
-    // 2) If the last key pressed was an operator, then change valueTwo.
-    state.valueTwo = state.valueTwo ? state.valueTwo + val : val;
+function handleNonZero(val) {
+  // If the last key pressed was an operator, then change valueTwo.
+  // Else change valueOne
+  if (STATE.operation) {
+    STATE.valueTwo = STATE.valueTwo ? STATE.valueTwo + val : val;
   } else {
-    // 3) If the last key pressed was not an operator, change valueOne.
-    state.valueOne = state.valueOne ? state.valueOne + val : val;
+    STATE.valueOne = STATE.valueOne ? STATE.valueOne + val : val;
   }
-
-  console.log("val1: ", state.valueOne, "opeartion: ", state.operation, "val2: ", state.valueTwo);
 }
 
 function handleZero() {
-  // 1) If state.operation is null, change Val2:
-  if (state.operation) {
-    state.valueTwo = state.valueTwo === 0 ? 0 : `${state.valueTwo + 0}`;
-    // : `${state.valueTwo}`.includes('.') ? state.valueTwo + 0 : 0;
+  // If the last key pressed was an operator, then change valueTwo.
+  // Else change valueOne
+  if (STATE.operation) {
+    STATE.valueTwo = STATE.valueTwo === 0 ? 0 : `${STATE.valueTwo + 0}`;
   }
-  // 2) Else change Val1:
   else {
-    state.valueOne = state.valueOne === 0 ? 0 : `${state.valueOne + 0}`;
-    // state.valueOne = `${state.valueOne}`.includes('.') ? state.valueOne + 0 : 0;
+    STATE.valueOne = STATE.valueOne === 0 ? 0 : `${STATE.valueOne + 0}`;
   }
-  console.log("val1: ", state.valueOne, "opeartion: ", state.operation, "val2: ", state.valueTwo);
-
 }
 
 // Decimal Button
@@ -83,25 +67,25 @@ const decimalButton = document.querySelector(".decimal");
 decimalButton.addEventListener("click", handleDecimal);
 
 function handleDecimal() {
-  // 1) Check if state.operator is null
-  if (state.operation) {
-    // 2) Change valueTwo
-    state.valueTwo = `${state.valueTwo}`.includes('.') ? state.valueTwo : state.valueTwo + '.';
+  // If the last key pressed was an operator, then change valueTwo.
+  // Else change valueOne
+  if (STATE.operation) {
+    STATE.valueTwo = `${STATE.valueTwo}`.includes('.') ? STATE.valueTwo : STATE.valueTwo + '.';
   }
   else {
-    state.valueOne = `${state.valueOne}`.includes('.') ? state.valueOne : state.valueOne + '.';
+    STATE.valueOne = `${STATE.valueOne}`.includes('.') ? STATE.valueOne : STATE.valueOne + '.';
   }
-  console.log("val1: ", state.valueOne, "opeartion: ", state.operation, "val2: ", state.valueTwo);
-
   handleDisplay();
-  state.lastKeyAnOperator = false;
+  STATE.lastKeyAnOperator = false;
 }
 
 // Operators
-// Plus
 const operatorButtons = document.querySelectorAll("[class*=op]")
 operatorButtons.forEach(button => {
+  operand = button.textContent;
+
   button.addEventListener("click", handleOperation)
+  // button.addEventListener("click", handleOperation)
 })
 
 function handleOperation(e) {
@@ -109,55 +93,54 @@ function handleOperation(e) {
 
   // 1) Check if there is a previous operation stored in memory && last key 
   //    pressed was not an operator:
-  if (state.operation && !state.lastKeyAnOperator) {
+  if (STATE.operation && !STATE.lastKeyAnOperator) {
     evaluateOperation();
   }
   setOperator(incomingOperation);
-  state.lastKeyAnOperator = true;
-  console.log("val1: ", state.valueOne, "opeartion: ", state.operation, "val2: ", state.valueTwo);
+  STATE.lastKeyAnOperator = true;
+  console.log("val1: ", STATE.valueOne, "opeartion: ", STATE.operation, "val2: ", STATE.valueTwo);
 }
 
-function setOperator(operator) {
+// function setOperator(operator) {
+//   switch (operator) {
+//     case "+": {
+//       STATE.operation = "add";
+//       break;
+//     }
+//     case "-": {
+//       STATE.operation = "subtract";
+//       break;
+//     }
+//     case "/": {
+//       STATE.operation = "divide";
+//       break;
+//     }
+//     case "*": {
+//       STATE.operation = "multiply";
+//       break;
+//     }
+//   }
+// }
+
+function operate(operator, num1, num2) {
   switch (operator) {
-    case "+": {
-      state.operation = "add";
+    case '+': {
+      add(num1, num2);
       break;
     }
-    case "-": {
-      state.operation = "subtract";
+    case '-': {
+      subtract(num1, num2);
       break;
     }
-    case "/": {
-      state.operation = "divide";
+    case '*': {
+      multiply(num1, num2);
       break;
     }
-    case "*": {
-      state.operation = "multiply";
-      break;
-    }
-  }
-}
-
-function evaluateOperation() {
-  switch (state.operation) {
-    case "add": {
-      state.valueOne = `${+state.valueOne + +state.valueTwo}`;
-      break;
-    }
-    case "subtract": {
-      state.valueOne = `${+state.valueOne - +state.valueTwo}`;
-      break;
-    }
-    case "multiply": {
-      state.valueOne = `${+state.valueOne * +state.valueTwo}`;
-      break;
-    }
-    case "divide": {
-      state.valueOne = `${+state.valueOne / +state.valueTwo}`;
+    case '/': {
+      divide(num1, num2);
       break;
     }
   }
-  state.valueTwo = 0;
 }
 
 // Handle Display
@@ -169,13 +152,33 @@ function handleDisplay() {
   // 3) If there is a saved operator and the last key pressed was an operator show val1
   // 4) If there is a save operator and the last key pressed was not an operator show val2
 
-  if (!state.operation) { // If there is no saved operator
-    mainDisplay.textContent = state.valueOne;
+  if (!STATE.operation) { // If there is no saved operator
+    mainDisplay.textContent = STATE.valueOne;
   }
-  else if (state.operation && state.lastKeyAnOperator) {
-    mainDisplay.textContent = state.valueOne;
+  else if (STATE.operation && STATE.lastKeyAnOperator) {
+    mainDisplay.textContent = STATE.valueOne;
   }
-  else if (state.operation && !state.lastKeyAnOperator) {
-    mainDisplay.textContent = state.valueTwo;
+  else if (STATE.operation && !STATE.lastKeyAnOperator) {
+    mainDisplay.textContent = STATE.valueTwo;
   }
 }
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "Cannot divide by zero"
+  }
+  return num1 / num2
+}
+
