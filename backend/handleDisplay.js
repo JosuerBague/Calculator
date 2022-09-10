1d94cb9f0d9c5f8142c5c2f294c072ca6dc2db5a
JosuerBague <josuer.online.work@gmail.com>
2022-09-10 14:45:41 +0800

    Move handleDisplay to separate module

---
+++ backend/handleDisplay.js
@@ -0,0 +1,18 @@
+export default function handleDisplay() {
+  const mainDisplay = document.querySelector(".display-main");
+
+  // 1) Check if there is a saved operator:
+  // 2) If there is no saved operator then show the value of val1
+  // 3) If there is a saved operator and the last key pressed was an operator show val1
+  // 4) If there is a save operator and the last key pressed was not an operator show val2
+
+  if (!STATE.operation) { // If there is no saved operator
+    mainDisplay.textContent = STATE.valueOne;
+  }
+  else if (STATE.operation && STATE.lastKeyAnOperator) {
+    mainDisplay.textContent = STATE.valueOne;
+  }
+  else if (STATE.operation && !STATE.lastKeyAnOperator) {
+    mainDisplay.textContent = STATE.valueTwo;
+  }
+}
