07bd056b3fdb07d15d7172723ea62f733a7e4bfb
JosuerBague <josuer.online.work@gmail.com>
2022-09-10 14:46:03 +0800

    Move hanldeOperation to separate module

---
+++ backend/handleOperation.js
@@ -0,0 +1,89 @@
+function handleOperation(e) {
+
+  // Check if there is a previous operation stored in memory && last key 
+  // pressed was not an operator, carry out the stored operation:
+  if (STATE.operation && !STATE.lastKeyAnOperator) {
+    operate(STATE.operation, STATE.valueOne, STATE.valueTwo);
+    handleDisplay()
+  }
+
+  setOperator(e.target.textContent);
+
+  console.log(STATE)
+}
+
+function setOperator(operator) {
+  switch (operator) {
+    case "+": {
+      STATE.operation = "+";
+      break;
+    }
+    case "-": {
+      STATE.operation = "-";
+      break;
+    }
+    case "/": {
+      STATE.operation = "/";
+      break;
+    }
+    case "*": {
+      STATE.operation = "*";
+      break;
+    }
+  }
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+': {
+      add(num1, num2);
+      break;
+    }
+    case '-': {
+      subtract(num1, num2);
+      break;
+    }
+    case '*': {
+      multiply(num1, num2);
+      break;
+    }
+    case '/': {
+      divide(num1, num2);
+      break;
+    }
+  }
+
+  STATE.operation = null
+}
+
+function add(num1, num2) {
+  STATE.total = +num1 + +num2;
+  STATE.valueOne = STATE.total;
+  STATE.valueTwo = 0;
+}
+
+function subtract(num1, num2) {
+  STATE.total = +num1 - +num2;
+  STATE.valueOne = STATE.total;
+  STATE.valueTwo = 0;
+}
+
+function multiply(num1, num2) {
+  STATE.total = +num1 * +num2;
+  STATE.valueOne = STATE.total;
+  STATE.valueTwo = 0;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "Cannot divide by zero"
+  }
+
+  STATE.total = +num1 / +num2;
+  STATE.valueOne = STATE.total;
+  STATE.valueTwo = 0;
+}
+
+
+
+export default handleOperation;
