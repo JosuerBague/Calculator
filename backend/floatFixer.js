c7a5f58e590b16a57ffa7912d8dbdcc8d082a844
JosuerBague <josuer.online.work@gmail.com>
2022-09-14 18:34:10 +0800

    Change exported function from test to floatFixer in floatFixer.js

--- backend/floatFixer.js
+++ backend/floatFixer.js
@@ -2,7 +2,7 @@
   Returns a multiple of ten (10, 100, 1000, 10000).
 */
 
-export default function test(data) {
+export default function floatFixer(data) {
   let temp = '1'
   for (let i = data.toString().length - 1; i >= 0; i--) {
     temp += '0';
