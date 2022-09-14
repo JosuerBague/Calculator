9bad7f91ba4968fd296efaa72bfd6f3689638361
JosuerBague <josuer.online.work@gmail.com>
2022-09-14 09:27:50 +0800

    Add getters and setters for STATE object properties

--- backend/state.js
+++ backend/state.js
@@ -8,4 +8,76 @@ export const STATE = {
   operation: null,
   lastKeyAnOperator: false, // Tracks if the last key pressed was an operator
   isPowered: false,
+
+  set total(val) {
+    this.total = val;
+  },
+
+  get total() {
+    return this.total;
+  },
+
+  set memoryTotal(val) {
+    this.setMemoryTotal = val;
+  },
+
+  get memoryTotal() {
+    return this.memoryTotal;
+  },
+
+  set overwrite(val) {
+    this.overwrite = val;
+  },
+
+  get overwrite() {
+    return this.overwrite;
+  },
+
+  set postOperation(val) {
+    this.postOperation = val;
+  },
+
+  get postOperation() {
+    return this.postOperation
+  },
+
+  set valueOne(val) {
+    this.valueOne = val;
+  },
+
+  get valueOne() {
+    return this.valueOne;
+  },
+
+  set valueTwo(val) {
+    this.valueTwo = val;
+  },
+
+  get valueTwo() {
+    return this.valueTwo;
+  },
+
+  set operation(val) {
+    this.operation = val;
+  },
+
+  get operation() {
+    return this.operation;
+  },
+
+  set lastKeyAnOperator(val) {
+    this.lastKeyAnOperator = val;
+  },
+
+  get lastKeyAnOperator() {
+    return this.lastKeyAnOperator;
+  },
+
+  set isPowered(val) {
+    this.isPowered = val;
+  },
+
+  get isPowered() {
+    return this.isPowered;
+  },
 }
