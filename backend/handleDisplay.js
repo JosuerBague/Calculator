9b9cb99a3a6d5d41a241ffc28073c36fee52133e
JosuerBague <josuer.online.work@gmail.com>
2022-09-15 17:20:29 +0800

    Implement format function inside handleDisplay

--- backend/handleDisplay.js
+++ backend/handleDisplay.js
@@ -4,16 +4,8 @@ import format from "./format.js";
 export default function handleDisplay() {
   const mainDisplay = document.querySelector(".display-main");
 
-
-
-  // 1) Check if there is a saved operator:
-  // 2) If there is no saved operator then show the value of val1
-  // 3) If there is a saved operator and the last key pressed was an operator show val1
-  // 4) If there is a save operator and the last key pressed was not an operator show val2
-
   if (!STATE.operation) {
     // If there is no saved operator, then the change is done on first operand:
-
     mainDisplay.textContent = format(STATE.valueOne);
   }
   else if (STATE.operation && !STATE.postOperation) {
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import { handleOperation } from "./handleOperation.js";
 import handleEquals from "./handleEquals.js";
 import { clearAll, recallMemory, memPlus, memSubtract } from "./memoryFunctions.js";
 import { STATE } from "./state.js";
-import test from "./floatFixer.js";
+import { truncate } from "./format.js";
 
 // Create global STATE object:
 
@@ -61,9 +61,3 @@ memSubtractButton.addEventListener('click', memSubtract);
 // Equals Button
 const equalsButton = document.querySelector('.equals');
 equalsButton.addEventListener('click', handleEquals);
-
-console.log(test("1.1"));
-console.log(test("20.12"));
-console.log(test("300.123"));
-console.log(test("4000.1234"));
-console.log(test("50000.12345"));
