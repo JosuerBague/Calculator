ef9a70c46643335a295a11469605d0b16f0696aa
JosuerBague <josuer.online.work@gmail.com>
2022-09-17 07:11:17 +0800

    Refactor handleDisplay

--- backend/handleDisplay.js
+++ backend/handleDisplay.js
@@ -2,24 +2,27 @@ import { STATE } from "./state.js";
 import format from "./format.js";
 
 export default function handleDisplay() {
+  const mainDisplay = document.querySelector(".display-main");
+
+  // If there is an error do nothing, unless all clear is clicked.
   if (STATE.isError) {
     return
   }
 
-  const mainDisplay = document.querySelector(".display-main");
+  // If the click happens post operation:
+  if (STATE.postOperation) {
+    mainDisplay.textContent = format(STATE.total);
+    return
+  }
 
+  // If there is no saved operator, then change the first operand:
   if (!STATE.operation) {
-    // If there is no saved operator, then the change is done on first operand:
     mainDisplay.textContent = format(STATE.valueOne);
+    return
   }
-  else if (STATE.operation && !STATE.postOperation) {
-    // If there is a saved operator, but it is not post operation
-    // It means we are changing the second operand
+  // If there is a saved operator, but not post operation,
+  // Change the 2nd value:
+  if (STATE.operation && !STATE.postOperation) {
     mainDisplay.textContent = format(STATE.valueTwo);
-  }
-  else if (STATE.operation && STATE.postOperation) {
-    // If there is a saved operation and it is post Operation
-    console.log(typeof STATE.valueOne);
-    mainDisplay.textContent = format(STATE.valueOne);
   }
 }
