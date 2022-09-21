7ddb20271c4b86233cc098d6132b6809b0173372
JosuerBague <josuer.online.work@gmail.com>
2022-09-21 17:33:55 +0800

    Refactor format.js for readability and functionality
    Format.js is now the main logic behind handling what the calculator displays.
    
    If the data provided is so large that an exponent is needed to represent it
    in computers, the calculator throws an error.
    
    If the data is too small, that it requires an exponent, the calculator truncates
    it and shows only the first 16 relevant digits.
    
    Format.js also contains the helper functions truncate and addCommas.

--- backend/format.js
+++ backend/format.js
@@ -1,33 +1,76 @@
-export default function format(data) {
-  // If the data contains a decimal
-  let [whole, fract] = truncate(data).split('.');
-  whole = addCommas(whole);
+import handleSmallNumbers from "./handleSmallNumbers.js";
+import { STATE } from "./state.js";
 
-  if (fract) {
-    return [whole, fract].join('.'); 0
+export default function format(data) {
+  // If the data does not exceed 16 digits:
+  if (data.replace('.', '').length <= 16) {
+    return addCommas(data)
   }
 
-  return whole;
+  // If the data display length exceeds 16 digits and contains a 
+  // positive exponent:
+  if (data.includes('+')) {
+    STATE._error = true;
+    return addCommas(data)
+  }
+
+  // If the data display length exceeds 16 and contains a 
+  // negative exponent call handleSmallNumbers with 
+  // truncate:
+  if (data.includes('-')) {
+    let formattedAns = addCommas(truncate(handleSmallNumbers(data)));
+    return formattedAns;
+  }
+
+  // If the data display length exceeds 16 but there is no exponent:
+  if (data.replace('.', '').length > 16) {
+    return addCommas(truncate(data));
+  }
 }
 
 export function truncate(data) {
-  let maxIndex = data.includes('.') ? 16 : 15;
+  const maxIndex = 16;
+  const valueAtMaxIndex = data[maxIndex];
+  const valueAfterMaxIndex = data[maxIndex + 1];
+  const indexOfDecimal = data.indexOf('.');
 
-  let valueAfterMaxIndex = data.slice(maxIndex + 1, maxIndex + 2);
-  let valueAtMaxIndex = data.slice(maxIndex, maxIndex + 1);
-  console.log(valueAfterMaxIndex, valueAtMaxIndex, data)
+  // If the value at maxIndex is not a nine simply round:
+  if (valueAtMaxIndex !== '9') {
+    return `${data.slice(0, maxIndex)}${valueAfterMaxIndex >= 5 ? parseInt(valueAtMaxIndex) + 1 : valueAtMaxIndex}`
+  }
 
+  else {
+    let indexNonNine = maxIndex;
 
-  return `${data.slice(0, maxIndex)}${valueAfterMaxIndex >= 5 ? parseInt(valueAtMaxIndex) + 1 : valueAtMaxIndex}`
+    while (data[indexNonNine] === '9' && indexNonNine !== 0) {
+      indexNonNine--
+    }
+
+    if (indexNonNine > indexOfDecimal && indexOfDecimal !== undefined) {
+      return `${data.slice(0, indexNonNine)}${parseInt(data[indexNonNine]) + 1}`
+    }
+
+    else if (indexNonNine < indexOfDecimal && indexOfDecimal !== undefined) {
+      let padZero = '';
+
+      for (let i = indexNonNine; i < indexOfDecimal; i++) {
+        padZero += '0'
+      }
+
+      return `${data.slice(0, indexNonNine)}${parseInt(data[indexNonNine]) + 1}${padZero}`
+    }
+  }
 }
 
 
 function addCommas(data) {
+
+  let [whole, fract] = data.split('.');
   let temp = '';
   let chunks = [];
 
-  for (let i = data.length - 1; i >= 0; i--) {
-    temp = data[i] + temp;
+  for (let i = whole.length - 1; i >= 0; i--) {
+    temp = whole[i] + temp;
 
     if (temp.length === 3) {
       chunks.push(temp);
@@ -39,7 +82,15 @@ function addCommas(data) {
     }
   }
 
-  return chunks.reverse().join(',')
+  let formatted = chunks.reverse().join(',');
+
+  if (fract) {
+    return [formatted, fract].join('.');
+  } else if (!fract && data.includes('.')) {
+    return formatted + '.';
+  } else {
+    return formatted;
+  }
 }
 
 
