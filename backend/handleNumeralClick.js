8e1805c1c83c9df51ba8bb42e18e3199906af26a
JosuerBague <josuer.online.work@gmail.com>
2022-09-21 17:57:07 +0800

    Remove unused old code

--- backend/handleNumeralClick.js
+++ backend/handleNumeralClick.js
@@ -2,21 +2,6 @@ import { STATE } from "./state.js";
 import handleDisplay from "./handleDisplay.js";
 
 function handleNumeralClick(e) {
-  // const mainDisplay = document.querySelector('.display-main');
-  // const maxDisplayLength = 16; // Digits only
-
-  // const currDisplay = mainDisplay.textContent.replace(/\W+/g, '');
-
-  // if (currDisplay.length <= maxDisplayLength) {
-  //   // Check if the number clicked is zero.
-  //   +e.target.textContent === 0 ? handleZero() : handleNonZero(e.target.textContent);
-  //   STATE.lastKeyAnOperator = false;
-  //   STATE.overwrite = false;
-  //   STATE.postOperation = false;
-  //   handleDisplay();
-  //   console.log(STATE)
-  // }
-
   // If the click happens post memory plus or minus (overwrite === true),
   // change valueTwo:
   if (STATE._overwrite) {
@@ -46,7 +31,5 @@ function handleNumeralClick(e) {
 function changeValue(name, input) {
   STATE[name] = STATE[name] === '0' ? input : STATE[name] + input;
 }
-
-
 
 export default handleNumeralClick;
