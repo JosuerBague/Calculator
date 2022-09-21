8bab667b4ea7cb44b3de13868015326ba355eec8
JosuerBague <josuer.online.work@gmail.com>
2022-09-21 20:14:29 +0800

    Refactor memoryPlus, memoryMinus, and recallMemory

--- backend/memoryFunctions.js
+++ backend/memoryFunctions.js
@@ -1,4 +1,5 @@
 import { STATE } from "./state.js";
+import handleDisplay from "./handleDisplay.js";
 
 function clearAll() {
   STATE.total = 0;
@@ -14,47 +15,40 @@ function clearAll() {
   mainDisplay.textContent = "CLEAR";
   setTimeout(() => {
     mainDisplay.textContent = 0;
-  }, 750)
-
-  console.log(STATE)
+  }, 750);
+  console.log(STATE);
 }
 
 function recallMemory() {
-  const mainDisplay = document.querySelector(".display-main");
-  mainDisplay.textContent = STATE.memoryTotal;
-
   // If there is an operation stored in memory, modify valueTwo,
   // else modify valueOne
-  if (STATE.operation) {
-    STATE.valueTwo = STATE.memoryTotal;
-  }
-  else {
-    STATE.valueOne = STATE.memoryTotal;
-  }
+  const name = STATE._operation ? "_valueTwo" : "_valueOne"
+  STATE[name] = STATE._memoryTotal;
+  handleDisplay("_memoryTotal")
 
   STATE.lastKeyAnOperator = false;
-  console.log(STATE)
+  console.log(STATE);
 }
 
 function memPlus() {
+  if (STATE._error) return;
+
   const mainDisplay = document.querySelector('.display-main');
 
-  STATE.memoryTotal = +STATE.memoryTotal + +mainDisplay.textContent
+  STATE._memoryTotal = (parseFloat(STATE._memoryTotal) + parseFloat(mainDisplay.textContent)).toString();
 
   STATE.overwrite = true;
   STATE.lastKeyAnOperator = false;
-
-  console.log(STATE)
+  console.log(STATE);
 }
 
 function memSubtract() {
   const mainDisplay = document.querySelector('.display-main');
 
-  STATE.memoryTotal = +STATE.memoryTotal - +mainDisplay.textContent;
+  STATE.memoryTotal = (parseFloat(STATE.memoryTotal) - parseFloat(mainDisplay.textContent)).toString();
 
   STATE.overwrite = true;
   STATE.lastKeyAnOperator = false;
-
   console.log(STATE);
 }
 
