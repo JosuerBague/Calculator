14b9b3ced2445054fa2ddc87f87630433b11b0a0
JosuerBague <josuer.online.work@gmail.com>
2022-09-21 09:25:46 +0800

    Refactor handleNumeralClick
    Moved the handleZero function to its own module. Replaced handleNonZero
    with changeValue helper function for better readability.

--- backend/handleNumeralClick.js
+++ backend/handleNumeralClick.js
@@ -2,50 +2,51 @@ import { STATE } from "./state.js";
 import handleDisplay from "./handleDisplay.js";
 
 function handleNumeralClick(e) {
-  const mainDisplay = document.querySelector('.display-main');
-  const maxDisplayLength = 16; // Digits only
+  // const mainDisplay = document.querySelector('.display-main');
+  // const maxDisplayLength = 16; // Digits only
 
-  const currDisplay = mainDisplay.textContent.replace(/\W+/g, '');
+  // const currDisplay = mainDisplay.textContent.replace(/\W+/g, '');
 
-  if (currDisplay.length <= maxDisplayLength + 1) {
-    // Check if the number clicked is zero.
-    +e.target.textContent === 0 ? handleZero() : handleNonZero(e.target.textContent);
-    STATE.lastKeyAnOperator = false;
-    STATE.overwrite = false;
-    STATE.postOperation = false;
+  // if (currDisplay.length <= maxDisplayLength) {
+  //   // Check if the number clicked is zero.
+  //   +e.target.textContent === 0 ? handleZero() : handleNonZero(e.target.textContent);
+  //   STATE.lastKeyAnOperator = false;
+  //   STATE.overwrite = false;
+  //   STATE.postOperation = false;
+  //   handleDisplay();
+  //   console.log(STATE)
+  // }
+
+  // If the click happens post memory plus or minus (overwrite === true),
+  // change valueTwo:
+  if (STATE._overwrite) {
+    changeValue('_valueTwo', e.target.textContent);
+    STATE._lastKeyAnOperator = false;
+    STATE._overwrite = false;
+    STATE._postOperation = false;
     handleDisplay();
-    console.log(STATE)
-  }
-}
-
-function handleNonZero(val) {
-  // Check if the number input happens right after invoking a memory function.
-  // In such a case the input should overwrite the current value stored.
-  if (STATE.overwrite) {
-    STATE.valueTwo = val;
-    STATE.overwrite = false;
-    return
+    return;
   }
 
-  // If there is an operation stored in memory, if true then change valueTwo.
-  // Else change valueOne
-  if (STATE.operation) {
-    STATE.valueTwo = STATE.valueTwo !== "0" ? STATE.valueTwo + val : val;
-
-  } else {
-    STATE.valueOne = STATE.valueOne !== "0" ? STATE.valueOne + val : val;
-  }
-}
-
-function handleZero() {
-  // If there is an operation stored in memory, if true then change valueTwo.
-  // Else change valueOne
-  if (STATE.operation) {
-    STATE.valueTwo = STATE.valueTwo === 0 ? 0 : `${STATE.valueTwo + 0}`;
+  // If there is no stored operation change valueOne:
+  if (!STATE._operation) {
+    changeValue('_valueOne', e.target.textContent);
   }
   else {
-    STATE.valueOne = STATE.valueOne === 0 ? 0 : `${STATE.valueOne + 0}`;
+    changeValue('_valueTwo', e.target.textContent);
   }
+  console.log(STATE)
+
+  STATE._lastKeyAnOperator = false;
+  STATE._overwrite = false;
+  STATE._postOperation = false;
+  handleDisplay();
 }
 
-export default handleChangeValue;
+function changeValue(name, input) {
+  STATE[name] = STATE[name] === '0' ? input : STATE[name] + input;
+}
+
+
+
+export default handleNumeralClick;
