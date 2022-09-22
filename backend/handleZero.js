36917438ade84a431e03b0bd850db7ceabbc3e25
JosuerBague <josuer.online.work@gmail.com>
2022-09-22 21:30:12 +0800

    Refactor for better readability

--- backend/handleZero.js
+++ backend/handleZero.js
@@ -2,20 +2,12 @@ import handleDisplay from "./handleDisplay.js";
 import { STATE } from "./state.js";
 
 export default function handleZero() {
-  console.log('clicked')
-  if (!STATE._operation) {
+  const name = STATE._operation ? "_valueTwo" : "_valueOne"
 
-    if (STATE._valueOne.replace('.', '').length === 16) return;
-    STATE._valueOne = STATE._valueOne === '0' ? '0' : STATE._valueOne + '0';
-  }
-  else {
-
-    if (STATE._valueTwo.replace('.', '').length === 16) return;
-    STATE._valueTwo = STATE._valueTwo === '0' ? '0' : STATE._valueTwo + '0';
-  }
+  STATE[name] = STATE[name] === '0' ? '0' : STATE[name] + '0';
 
   STATE._lastKeyAnOperator = false;
   STATE._overwrite = false;
   STATE._postOperation = false;
-  handleDisplay();
+  handleDisplay(name);
 }
