acea9049002d6434ba6cb725d511457ea7ddcd3e
JosuerBague <josuer.online.work@gmail.com>
2022-09-24 21:31:26 +0800

    Add clearAll functionality to power off

--- backend/togglePower.js
+++ backend/togglePower.js
@@ -1,16 +1,18 @@
+import { clearAll } from "./memoryFunctions.js";
 import { STATE } from "./state.js"
 
 export default function togglePower() {
   const memoryIndicator = document.querySelector(".display-memory");
   const mainDisplay = document.querySelector(".display-main");
 
-  if (STATE.isPowered) {
+  if (STATE._isPowered) {
     memoryIndicator.style.opacity = 1;
     mainDisplay.style.opacity = 1;
   } else {
+    clearAll
     memoryIndicator.style.opacity = 0;
     mainDisplay.style.opacity = 0;
   }
 
-  STATE.isPowered = !STATE.isPowered;
+  STATE._isPowered = !STATE._isPowered;
 }
